60dd71448e2249d10fa081dafbfc016b73bca56f
szamon <szamon@wp.pl>
2019-08-07 10:48:31 +0200

    Updates

---
+++ precache-manifest.c5e2843630fdbe1e9a31b9b12dcb23b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "167c0a87e035f753244f030d7d8b38a9",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "78c09ff7c14350ba64b4",
+    "url": "/Calculator/static/css/main.f282c589.chunk.css"
+  },
+  {
+    "revision": "7cd5468078641c3362a4",
+    "url": "/Calculator/static/js/2.8a3a987c.chunk.js"
+  },
+  {
+    "revision": "78c09ff7c14350ba64b4",
+    "url": "/Calculator/static/js/main.4813961f.chunk.js"
+  },
+  {
+    "revision": "cf49d768c4d3a5b9fc25",
+    "url": "/Calculator/static/js/runtime~main.8078b280.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
